812f47a70c702c095bf3febc3addbfa87141dd9e
Luis Gregorio Navarro <lgnr.archive@gmail.com>
2019-11-12 06:55:31 -0600

    Updates

---
+++ precache-manifest.6486c5a6f074f20048393f2d09b74b84.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525c11635eb404959d891f451b2cea5b",
+    "url": "/hiit/index.html"
+  },
+  {
+    "revision": "313b2c15471f191b1abc",
+    "url": "/hiit/static/js/2.dfbb697a.chunk.js"
+  },
+  {
+    "revision": "6e843a098f3041fdb0aa",
+    "url": "/hiit/static/js/main.6f66473b.chunk.js"
+  },
+  {
+    "revision": "05a98f18c9d280ef62f0",
+    "url": "/hiit/static/js/runtime-main.115d3185.js"
+  }
+]);
